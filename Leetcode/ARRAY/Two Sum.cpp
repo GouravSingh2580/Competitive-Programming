e7609aac78a2084488f085ca6b2736771c9867ce
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2020-04-11 14:16:21 +0530

    Add files via upload

---
+++ Leetcode/ARRAY/Two Sum.cpp
@@ -0,0 +1,29 @@
+/*
+Runtime: 564 ms, faster than 10.10% of C++ online submissions for Two Sum.
+Memory Usage: 7 MB, less than 100.00% of C++ online submissions for Two Sum.
+
+
+AC in 7th Try
+*/
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    a=i;
+                    b=j;
+                }
+            }
+        }
+        return {a,b};
+    }
+};
